a7ddfc7464fb71988ef8304e3719708c3637b747
Jose Socorro <jcarlossoorro@gmail.com>
2023-03-11 20:26:58 -0500

    Add ticket card

--- frontend/src/components/TicketCard.js
+++ frontend/src/components/TicketCard.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const TicketCard = () => {
-  return <div>TicketCard</div>;
+const TicketCard = ({
+  contact,
+  subject,
+  group,
+  agent,
+  priority,
+  status,
+  ticket_number,
+}) => {
+  return (
+    <tr className="ticket-card-container">
+      <td>
+        <input type="checkbox" name="select" /> {contact}
+      </td>
+
+      <td>
+        <Link to={"/ticket/" + ticket_number}>
+          {subject} <span className="light-font">{" #" + ticket_number}</span>{" "}
+        </Link>
+      </td>
+
+      <td> {group} </td>
+      <td> {agent} </td>
+      <td> {priority} </td>
+      <td> {status} </td>
+    </tr>
+  );
 };
 
 export default TicketCard;
--- frontend/src/pages/Tickets.js
+++ frontend/src/pages/Tickets.js
@@ -1,17 +1,32 @@
 import React from "react";
 import NavBar from "../components/NavBar";
 import TopBar from "../components/TopBar";
+import TicketCard from "../components/TicketCard";
 
 const Tickets = () => {
   return (
-    <div>
+    <div className="ticket-page">
       <NavBar />
       <TopBar title="Tickets" />
       <div className="bottom-bar">
         <span>bottom-bar</span>
       </div>
 
-      <div className="tickets-container"></div>
+      <div className="tickets-container">
+        <table>
+          <tbody>
+            <TicketCard
+              contact="Alfonso Davie"
+              subject="CANNOT USE PANO"
+              group="Remote support"
+              agent="Jose Socorro"
+              priority="High"
+              status="Open"
+              ticket_number="1"
+            />
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };

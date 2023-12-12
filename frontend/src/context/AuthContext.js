97042675df98628c4860099aaf07aa3b70583cf3
Jose <jcarlossoorro@gmail.com>
2023-12-12 13:53:59 -0500

    Add loading screen, this is to show people the server is restarting due to inactivity

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -84,7 +84,39 @@ export function AuthContextProvider({ children }) {
   return (
     <AuthContext.Provider
       value={{ ...state, dispatch, addTicketAssigned, updateTicketsAssigned }}>
-      {loaded && children}
+      {/* {loaded && children} */}
+
+      {loaded ? (
+        children
+      ) : (
+        <div className="flex flex-col justify-center items-center h-screen text-2xl text-center">
+          <p className="text-gray-800">
+            Getting back up, the backend server spins down after 15 minutes of
+            inactivity.
+          </p>
+          <p className="mt-6 text-gray-600">
+            This may take a few seconds, even a minute :(
+          </p>
+
+          <svg
+            class="mt-6 animate-spin -ml-1 mr-3 h-12 w-12 text-green-600 "
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24">
+            <circle
+              class="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"></circle>
+            <path
+              class="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+          </svg>
+        </div>
+      )}
     </AuthContext.Provider>
   );
 }
